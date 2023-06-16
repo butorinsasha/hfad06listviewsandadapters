a00bb83a1b38c3bccaa87391925f55e463c20825
pushkinaleksandr <butorinsasha@gmail.com>
2023-06-16 04:34:55 +0300

    TopLevelActivity.java realization of OnItemClickListener and set to listView

--- app/src/main/java/local/hfad/hfad06listviewsandadapters/TopLevelActivity.java
+++ app/src/main/java/local/hfad/hfad06listviewsandadapters/TopLevelActivity.java
@@ -1,7 +1,13 @@
 package local.hfad.hfad06listviewsandadapters;
 
+import static android.view.View.*;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        //Create an OnItemClickListener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    TopLevelActivity.this.startActivity(intent);
+                }
+            }
+        };
+
+        //Add the listener to the list view
+        ListView listView = findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
