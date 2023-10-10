353e30c1df0a28cea19b7084c6e54a7a54509668
a.b.butorin <a.b.butorin@tinkoff.ru>
2023-10-10 15:51:56 +0300

    Chapter 11. Just created an empty StarbuzzDataBaseHelper

---
+++ app/src/main/java/local/hfad/hfad06listviewsandadapters/StarbuzzDataBaseHelper.java
@@ -0,0 +1,26 @@
+package local.hfad.hfad06listviewsandadapters;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class StarbuzzDataBaseHelper extends SQLiteOpenHelper {
+
+    public StarbuzzDataBaseHelper(Context context,
+                                  @Nullable String name,
+                                  @Nullable SQLiteDatabase.CursorFactory factory,
+                                  int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
+}
