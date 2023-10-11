a25e8a4f97f5335106906ab574ad30314e7f5473
pushkinaleksandr <butorinsasha@gmail.com>
2023-10-11 03:04:22 +0300

    insert() into table DRINK

--- app/src/main/java/local/hfad/hfad06listviewsandadapters/StarbuzzDataBaseHelper.java
+++ app/src/main/java/local/hfad/hfad06listviewsandadapters/StarbuzzDataBaseHelper.java
@@ -1,8 +1,10 @@
 package local.hfad.hfad06listviewsandadapters;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 
 public class StarbuzzDataBaseHelper extends SQLiteOpenHelper {
@@ -14,7 +16,14 @@ public class StarbuzzDataBaseHelper extends SQLiteOpenHelper {
                                                     + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                                                     + "NAME TEXT, "
                                                     + "DESCRIPTION TEXT, "
-                                                    + "IMAGE_REAOURCE_ID INTEGER);";
+                                                    + "IMAGE_RESOURCE_ID INTEGER);";
+
+    private String insertIntoDrink(String name, String description, String imageResourceId) {
+        return "INSERT INTO DRINK (NAME, DESCRIPTION, IMAGE_REAOURCE_ID)"
+                + "VALUES"
+                + "('" + name + "', '" + description + "', '" + imageResourceId + "')";
+    }
+
 
     public StarbuzzDataBaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -27,7 +36,16 @@ public class StarbuzzDataBaseHelper extends SQLiteOpenHelper {
                                 + "NAME TEXT, "
                                 + "DESCRIPTION TEXT, "
                                 + "IMAGE_REAOURCE_ID INTEGER);");
+
+//        sqLiteDatabase.execSQL(insertIntoDrink("Latte", "A couple of espresso with streamed milk", R.drawable.latte));
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("NAME", "latte");
+        drinkValues.put("DESCRIPTION", "Espresso and steamed milk);");
+        drinkValues.put("IMAGE_RESOURCE_ID", "R.drawable.latte");
+        sqLiteDatabase.insert("DRINK", null, drinkValues);
     }
+
+
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
