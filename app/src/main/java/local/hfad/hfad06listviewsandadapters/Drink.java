3375de78ab9777885eeac73d3fc15261abf28cf1
a.b.butorin <a.b.butorin@tinkoff.ru>
2023-11-17 13:37:27 +0300

    Added Americano to Drink.java

--- app/src/main/java/local/hfad/hfad06listviewsandadapters/Drink.java
+++ app/src/main/java/local/hfad/hfad06listviewsandadapters/Drink.java
@@ -1,14 +1,15 @@
 package local.hfad.hfad06listviewsandadapters;
 
 public class Drink {
-    private String name;
-    private String description;
-    private int imageResourceId;
+    private final String name;
+    private final String description;
+    private final int imageResourceId;
 
     public static final Drink[] drinksArray = {
             new Drink("Latte", "A couple of espresso with streamed milk", R.drawable.latte),
             new Drink("Cuppuccino", "Espresso, hot milk, and a steamed milk foam", R.drawable.cappuccino),
-            new Drink("Filter", "Highest quality beans roasted and brewed fresh", R.drawable.filter)
+            new Drink("Filter", "Highest quality beans roasted and brewed fresh", R.drawable.filter),
+            new Drink("Americano", "No milk foam, just coffee and milk if you wish", R.drawable.americano)
     };
 
     public Drink(String name, String description, int imageResourceId) {
